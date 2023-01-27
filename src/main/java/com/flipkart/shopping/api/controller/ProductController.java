151e6c9cb9e1a72ef1337dd51182270347808068
Rahul Mittal <rahulmittal1167@gmail.com>
2023-01-27 23:36:37 +0530

    Added Logger

--- src/main/java/com/flipkart/shopping/api/controller/ProductController.java
+++ src/main/java/com/flipkart/shopping/api/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.flipkart.shopping.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import com.flipkart.shopping.api.model.Product;
 import com.flipkart.shopping.api.service.ProductService;
 
 @RestController
+@Slf4j
 public class ProductController {
 
 	@Autowired
@@ -19,13 +21,15 @@ public class ProductController {
 	
 	@GetMapping("/product/{id}")
 	public Product getProductById(@PathVariable int id) {
-		
+		log.info("Getting Product by Id {} ", id);
 		Optional<Product> product = service.findtheProductById(id);
+		log.info("Product Returned value is {}", product);
 		return product!=null ? product.get() : null;
 	}
 	
 	@GetMapping("/getAllproducts")
 	public List<Product> getProducts(){
+		log.info("Getting all the Products ######3");
 		return service.getAllTheProducts();
 	}
 }
--- src/main/java/com/flipkart/shopping/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/flipkart/shopping/api/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.flipkart.shopping.api.service.impl;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import com.flipkart.shopping.api.repository.UserRepo;
 import com.flipkart.shopping.api.service.UserService;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
 
 	@Autowired
@@ -18,27 +20,32 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public User findUserByUserName(String username) {
-		// TODO Auto-generated method stub
+		log.info("Finding user by username {}", username);
 		if(username!=null)
 			return repo.findByUsername(username);
+		else
+			log.warn("Username is null");
 		
 		return null;
 	}
 	
 	public User findUserByUserNameAndPassword(String username, String password) {
-		// TODO Auto-generated method stub
+		log.info("Find User By Username and Password");
 		if(username!=null && password!=null)
 			return repo.findByUsernameAndPassword(username, password);
-		
+		else
+			log.warn("Username and Password both are null");
 		return null;
 	}
 
 	@Override
 	public User saveUser(User user) {
-		// TODO Auto-generated method stub
+		log.info("Saving the User in DB ");
 		User savedUser = new User();
 		if(user!=null) {
 			savedUser = repo.save(user);
+		}else {
+			log.warn("User is Saved");
 		}
 		return savedUser;
 	}
